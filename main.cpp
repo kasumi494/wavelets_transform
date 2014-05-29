a73a20353204bb7c155a6607533af4e872722898
kasumi <borovitina.ekaterina@gmail.com>
2014-05-29 15:05:02 +0600

    [fix] restore compression

--- main.cpp
+++ main.cpp
@@ -4,34 +4,32 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "haartransformator.hpp"
+#include "daubechiestransformator.hpp"
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        std::cerr << "Usage: " << argv[0]
-                  << "image {show}" << std::endl;
-        return -1;
-    }
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0]
+              << "image leave_coeff" << std::endl;
+    return -1;
+  }
 
     cv::Mat original = cv::imread(argv[1], 1);
-//    cv::imshow("Original", original);
+    cv::imshow("Original", original);
 
     HaarTransformator haar_1(original);
-    haar_1.Show();
     cv::Mat_<cv::Vec3f> output_haar;
     haar_1.Decompose(output_haar, false);
-//    cv::imshow("Decomposition", output_haar);
 
     HaarTransformator haar_compressed(output_haar);
-    haar_compressed.Compress(output_haar);
-//    cv::imshow("Compressed", output_haar);
+    haar_compressed.Compress(output_haar, (size_t)atoi(argv[2]));
+    cv::imshow("Compressed", output_haar);
 
     HaarTransformator haar_2(output_haar);
     cv::Mat reconstruct;
     haar_2.Reconstruct(reconstruct, false);
     cv::imshow("Reconstruction", reconstruct);
 
-    cv::waitKey(0);
-    return 0;
+  cv::waitKey(0);
+  return 0;
 }
