d854d84fd15b3c1a42e683c6127d4a338cebfa68
kasumi <borovitina.ekaterina@gmail.com>
2014-05-29 15:40:53 +0600

    change style of main

--- main.cpp
+++ main.cpp
@@ -13,21 +13,21 @@ int main(int argc, char **argv)
     return -1;
   }
 
-    cv::Mat original = cv::imread(argv[1], 1);
-    cv::imshow("Original", original);
+  cv::Mat original = cv::imread(argv[1], 1);
+  cv::imshow("Original", original);
 
-    HaarTransformator haar_1(original);
-    cv::Mat_<cv::Vec3f> output_haar;
-    haar_1.Decompose(output_haar, false);
+  HaarTransformator haar_1(original);
+  cv::Mat_<cv::Vec3f> output_haar;
+  haar_1.Decompose(output_haar, false);
 
-    HaarTransformator haar_compressed(output_haar);
-    haar_compressed.Compress(output_haar, (size_t)atoi(argv[2]));
-    cv::imshow("Compressed", output_haar);
+  HaarTransformator haar_compressed(output_haar);
+  haar_compressed.Compress(output_haar, (size_t)atoi(argv[2]));
+  cv::imshow("Compressed", output_haar);
 
-    HaarTransformator haar_2(output_haar);
-    cv::Mat reconstruct;
-    haar_2.Reconstruct(reconstruct, false);
-    cv::imshow("Reconstruction", reconstruct);
+  HaarTransformator haar_2(output_haar);
+  cv::Mat reconstruct;
+  haar_2.Reconstruct(reconstruct, false);
+  cv::imshow("Reconstruction", reconstruct);
 
   cv::waitKey(0);
   return 0;
