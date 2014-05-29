154309baeb6974a5630ca61ff2d484ad33c67305
kasumi <borovitina.ekaterina@gmail.com>
2014-05-29 17:39:31 +0600

    d4 reconstruction

--- daubechiestransformator.cpp
+++ daubechiestransformator.cpp
@@ -14,12 +14,12 @@ void D4Transformator::Decompose(cv::Mat &output, bool isShow)
   Mat_<Vec3f> tmp = image_.clone();
   tmp /= sqrt(image_.total());
 
-  for (int lim_size = tmp.cols; lim_size >= 2; lim_size /= 2) {
+  for (int lim_size = tmp.cols; lim_size >= 4; lim_size /= 2) {
     decompose_row(tmp, lim_size);
     if (isShow) ShowNormalized(tmp);
   }
 
-  for (int lim_size = tmp.rows; lim_size >= 2; lim_size /= 2) {
+  for (int lim_size = tmp.rows; lim_size >= 4; lim_size /= 2) {
     decompose_col(tmp, lim_size);
     if (isShow) ShowNormalized(tmp);
   }
@@ -30,7 +30,22 @@ void D4Transformator::Decompose(cv::Mat &output, bool isShow)
 
 void D4Transformator::Reconstruct(cv::Mat &output, bool isShow)
 {
+  Mat_<Vec3f> tmp = image_.clone();
 
+  for (int lim_size = 4; lim_size <= tmp.rows; lim_size *= 2) {
+    reconstruct_col(tmp, lim_size);
+    if (isShow) ShowNormalized(tmp);
+  }
+
+  for (int lim_size = 4; lim_size <= tmp.cols; lim_size *= 2) {
+    reconstruct_row(tmp, lim_size);
+    if (isShow) ShowNormalized(tmp);
+  }
+
+  tmp *= sqrt(image_.total());
+  tmp.copyTo(output);
+  normalize(tmp, output, 0, 255, cv::NORM_MINMAX, -1, cv::Mat());
+  output.convertTo(output, CV_8U);
 }
 
 
@@ -87,11 +102,51 @@ void D4Transformator::decompose_col(cv::Mat_<cv::Vec3f> &output, int lim)
 
 void D4Transformator::reconstruct_row(cv::Mat_<cv::Vec3f> &output, int lim)
 {
+  int half = std::floor(lim * 0.5f);
+  Mat_ <Vec3f> tmp = Mat_ <Vec3f>(1, lim);
 
+  for (int k = 0; k < output.rows; ++k) {
+    tmp(0) = output(k, half - 1) * c_[2] + output(k, lim - 1) * c_[1] +
+             output(k, 0) * c_[0] + output(k, half) * c_[3];
+
+    tmp(1) = output(k, half - 1) * c_[3] - output(k, lim - 1) * c_[0] +
+             output(k, 0) * c_[1] - output(k, half) * c_[2];
+
+    for (int i = 0; i < half - 1; ++i) {
+      int w = 2 * i;
+      tmp(w + 2) = output(k, i) * c_[2] + output(k, i + half) * c_[1] +
+                   output(k, i + 1) * c_[0] + output(k, i + half + 1) * c_[3];
+
+      tmp(w + 3) = output(k, i) * c_[3] - output(k, i + half) * c_[0] +
+                   output(k, i + 1) * c_[1] - output(k, i + half + 1) * c_[2];
+    }
+
+    for (int i = 0; i < lim; ++i)   output(k, i) = tmp(i);
+  }
 }
 
 
 void D4Transformator::reconstruct_col(cv::Mat_<cv::Vec3f> &output, int lim)
 {
+  int half = std::floor(lim * 0.5f);
+  Mat_ <Vec3f> tmp = Mat_ <Vec3f>(1, lim);
 
+  for (int k = 0; k < output.cols; ++k) {
+    tmp(0) = output(half - 1, k) * c_[2] + output(lim - 1, k) * c_[1] +
+             output(0, k) * c_[0] + output(half, k) * c_[3];
+
+    tmp(1) = output(half - 1, k) * c_[3] - output(lim - 1, k) * c_[0] +
+             output(0, k) * c_[1] - output(half, k) * c_[2];
+
+    for (int i = 0; i < half - 1; ++i) {
+      int w = 2 * i;
+      tmp(w + 2) = output(i, k) * c_[2] + output(i + half, k) * c_[1] +
+                   output(i + 1, k) * c_[0] + output(i + half + 1, k) * c_[3];
+
+      tmp(w + 3) = output(i, k) * c_[3] - output(i + half, k) * c_[0] +
+                   output(i + 1, k) * c_[1] - output(i + half + 1, k) * c_[2];
+    }
+
+    for (int i = 0; i < lim; ++i)   output(i, k) = tmp(i);
+  }
 }
